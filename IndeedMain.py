f4eb408a0e7eb752ce1cde0ddcba479b4217892b
ABDURRAHMAN SALIH <92715976+salih-17@users.noreply.github.com>
2022-08-04 06:06:22 +0300

    filterdate = 1

--- IndeedMain.py
+++ IndeedMain.py
@@ -19,7 +19,7 @@ my_conn = create_engine("mysql+pymysql://admin:12345678@database-1.ciaff8ckhmlj.
 worldwidelinks = pd.read_csv ('worldwidelink.csv').set_index ('CountryName')
 #------------------------------------------------------------
 
-filterdate = 2
+filterdate = 1
 position = 'data'
 totalpostion = 0
 
