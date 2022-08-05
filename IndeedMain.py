4a5b9a15790e1a330260e38958b461fe5938b352
ABDURRAHMAN SALIH <92715976+salih-17@users.noreply.github.com>
2022-08-05 11:07:12 +0300

    Data1.csv

--- IndeedMain.py
+++ IndeedMain.py
@@ -217,7 +217,7 @@ def main():
     df2 = pd.DataFrame(ff)
     df2.to_sql (con =my_conn , name = 'IndeedDataSet5' , if_exists = 'append' , index = False )
     #file_name = str(int(random.random()*12345)) + "_df.xlsx"
-    df2.to_excel("Data1.xlsx")
+    df2.to_csv("Data1.csv")
     
 if __name__ == '__main__':
   main()
