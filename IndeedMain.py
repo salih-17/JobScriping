7932c0c0d272092d96ed8cde7dae631b49940ce3
ABDURRAHMAN SALIH <92715976+salih-17@users.noreply.github.com>
2022-08-08 06:31:04 +0300

    data analysis

--- IndeedMain.py
+++ IndeedMain.py
@@ -20,7 +20,7 @@ worldwidelinks = pd.read_csv ('worldwidelink.csv').set_index ('CountryName')
 #------------------------------------------------------------
 
 filterdate = 1
-position = 'frontend'
+position = 'data analysis'
 totalpostion = 0
 
 #------------------------------------------------------------
