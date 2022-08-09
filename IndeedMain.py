baef6005d4677c9c0d49b46a1f0440fb5c57c19b
ABDURRAHMAN SALIH <92715976+salih-17@users.noreply.github.com>
2022-08-09 08:32:01 +0300

    frontend%20developer

--- IndeedMain.py
+++ IndeedMain.py
@@ -20,7 +20,7 @@ worldwidelinks = pd.read_csv ('worldwidelink.csv').set_index ('CountryName')
 #------------------------------------------------------------
 
 filterdate = 1
-position = 'data'
+position = 'frontend%20developer'
 totalpostion = 0
 
 #------------------------------------------------------------
